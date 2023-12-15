1731bba7d15e24e0492a3064f5d09861d9ceb2b0
firodriguezb <stonescorpion10@hotmail.com>
2023-12-14 21:33:20 -0600

    Instalación Passport

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\OAuthController;
+use Laravel\Socialite\Facades\Socialite;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,4 +18,19 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+//API OAuth
+Route::group([
+    'prefix' => 'auth'
+], function () {
+    Route::post('login', [OAuthController::class, 'login']);
+    Route::post('signup', [OAuthController::class, 'signUp']);
+
+    Route::group([
+        'middleware' => 'auth:api'
+    ], function() {
+        Route::get('logout', [OAuthController::class, 'logout']);
+        Route::get('user', [OAuthController::class, 'user']);
+    });
 });
